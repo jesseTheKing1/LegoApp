a09b7d1694b8783b2e67ada356a78cdef0cdca89
jesse <jesseferguson17@yahoo.com>
2026-01-10 16:22:30 -0500

    frontend styling

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -130,17 +130,6 @@ function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
   );
 }
 
-function Select(props: React.SelectHTMLAttributes<HTMLSelectElement>) {
-  return (
-    <select
-      {...props}
-      className={cx(
-        "w-full rounded-xl border border-neutral-200 bg-white px-3 py-2 text-sm outline-none",
-        "focus:border-neutral-400 focus:ring-4 focus:ring-neutral-100"
-      )}
-    />
-  );
-}
 
 function Textarea(props: React.TextareaHTMLAttributes<HTMLTextAreaElement>) {
   return (
