02a1017c324522541b0142f81be8f4434e6861cc
jesse <jesseferguson17@yahoo.com>
2026-01-10 11:07:32 -0500

    configuring connection between django and react

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
 
     "rest_framework",
     "corsheaders",
-
 ]
 
 REST_FRAMEWORK = {
@@ -46,8 +45,9 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +60,10 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'config.urls'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://YOUR-REACT-SITE.onrender.com",
+    "https://YOUR-REACT-SITE.onrender.com", 
+    
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 TEMPLATES = [
     {
