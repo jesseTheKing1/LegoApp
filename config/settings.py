5d030b20fe9cd18717f52832de80a77180d0e6e6
jesse <jesseferguson17@yahoo.com>
2026-01-10 14:48:01 -0500

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -14,8 +14,11 @@ SECRET_KEY = 'django-insecure-7d3ky-upx!ddgef568kcwittzs9^-zydiod(c#9@zbd3szyc%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "legoapp-web.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 AUTH_USER_MODEL = "accounts.User"
 
 
