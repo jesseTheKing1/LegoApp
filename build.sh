2df1e858b127752fcf98a60a24c412574b2072f6
jesse <jesseferguson17@yahoo.com>
2026-01-12 20:09:11 -0500

    having issues with migrations

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate --fake-initial
 
-
+python manage.py dbshell
+DROP TABLE IF EXISTS parts_color CASCADE;
