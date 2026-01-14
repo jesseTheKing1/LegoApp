5adad9576526e97ecc812de928b0e05f7421f8e1
jesse <jesseferguson17@yahoo.com>
2026-01-14 16:58:13 -0500

    having issues with migrations

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-# python manage.py migrate
-# python manage.py bootstrap_superuser
+python manage.py migrate
+python manage.py bootstrap_superuser
